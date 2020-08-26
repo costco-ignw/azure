a6f5c999103bc1b12fb9fafea2b5b5a32993e23d
Dan <dan@xentaurs.com>
2020-08-26 10:52:51 -0500

    dan first commit

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ resource "azurerm_resource_group" "costco" {
   location = "westus"
 }
 
-
+terraform {
+  backend "azurerm"
+  resource_group_name  = azurerm_resource_group.costco.name
+  storage_account_name = "ignw"
+  containter_name      = "ignw"
+  key                  = "terraform.state"
+}
