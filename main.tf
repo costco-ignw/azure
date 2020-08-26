bb144a6afd1d597385c02fc41530687fff0e3636
Dan <dan@xentaurs.com>
2020-08-26 13:23:16 -0500

    adding tf backend

--- main.tf
+++ main.tf
@@ -4,10 +4,14 @@ resource "azurerm_resource_group" "costco" {
   location = "westus"
 }
 
+
 terraform {
-  backend "azurerm"
-  resource_group_name  = azurerm_resource_group.costco.name
-  storage_account_name = "ignw"
-  containter_name      = "ignw"
-  key                  = "terraform.state"
+  backend "azurerm" {
+    resource_group_name  = "costco"
+    storage_account_name = "ignw"
+    container_name       = "ignw"
+    subscription_id      = "a5e1f313-dcc0-4487-b6b6-ca9a628c4e87"
+    key                  = "terraform.tfstate"
+    access_key           = "LtT5h9p0LqReA/W8YlTpDrelUSlLXZFki+YzWPuFgaAmkKeZ+ztGi/6o1+ypzHOhJkCcOc4oC6wJIihOiWb/Fw=="
+  }
 }
