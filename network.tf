8d9810944e4dc146e48d30ab2a2bba490130c026
Dan <dan@xentaurs.com>
2020-08-26 10:21:50 -0500

    dan first commit

--- network.tf
+++ network.tf
@@ -1,21 +1,21 @@
-resource "azurerm_virtual_network" "tesla" {
-  name                = "tesla"
-  address_space       = ["10.20.0.0/16"]
-  location            = azurerm_resource_group.tesla.location
-  resource_group_name = azurerm_resource_group.tesla.name
+resource "azurerm_virtual_network" "costco" {
+  name                = "costco"
+  address_space       = ["10.30.0.0/16"]
+  location            = azurerm_resource_group.costco.location
+  resource_group_name = azurerm_resource_group.costco.name
 }
 
 
-resource "azurerm_subnet" "web" {
-  name                 = "web"
-  resource_group_name  = azurerm_resource_group.tesla.name
-  virtual_network_name = azurerm_virtual_network.tesla.name
-  address_prefix       = "10.20.2.0/24"
-}
+# resource "azurerm_subnet" "web" {
+#   name                 = "web"
+#   resource_group_name  = azurerm_resource_group.tesla.name
+#   virtual_network_name = azurerm_virtual_network.tesla.name
+#   address_prefix       = "10.20.2.0/24"
+# }
 
-resource "azurerm_subnet" "app" {
-  name                 = "app"
-  resource_group_name  = azurerm_resource_group.tesla.name
-  virtual_network_name = azurerm_virtual_network.tesla.name
-  address_prefix       = "10.20.3.0/24"
-}
+# resource "azurerm_subnet" "app" {
+#   name                 = "app"
+#   resource_group_name  = azurerm_resource_group.tesla.name
+#   virtual_network_name = azurerm_virtual_network.tesla.name
+#   address_prefix       = "10.20.3.0/24"
+# }
