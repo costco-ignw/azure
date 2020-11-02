b46ad28a9bd94c82a0c71cf147228ec8e25bdee2
Dan <dan@xentaurs.com>
2020-11-02 17:34:59 -0600

    blah

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "sql-mi-poc"
-    storage_account_name = "sqlmistorage1"
-    container_name       = "tfstate"
-    key                  = "npprod.terraform.tfstate"
+  backend “azurerm” {
+    storage_account_name = “sqlmistorage1"
+    container_name       = “tfstate”
+    key                  = “prod.terraform.tfstate”
+    use_msi              = true
+    subscription_id      = “b07cf94e-ce83-4d2d-99d6-28ce1a4daf7a”
+    tenant_id            = “b81d7c17-75bd-4272-8b31-171ea6c31427"
   }
 }
