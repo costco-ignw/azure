f63e713c3462d8be2d794379f571c679c0e402bf
Dan <dan@xentaurs.com>
2020-11-02 17:02:48 -0600

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = azurerm_resource_group.rg.name
+    resource_group_name  = "sql-mi-poc"
     storage_account_name = "sqlmistorage1"
     container_name       = "tfstate"
     key                  = "npprod.terraform.tfstate"
