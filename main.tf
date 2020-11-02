99e60148ec220d0d983b2657a2426c6c707e4a2f
Dan <dan@xentaurs.com>
2020-11-02 17:51:13 -0600

    blah

--- main.tf
+++ main.tf
@@ -18,16 +18,16 @@ module "vnet" {
   source              = "./Modules/sqlmi-vnet"
   resource_group_name = azurerm_resource_group.rg.name
   #route_table_name    = "sql-mi-routetable"
-  vnet_name           = "sql-mi-vnet"
+  vnet_name = "sql-mi-vnet"
   #vnet_address_space  = "10.0.0.0/16"
-  location            = "West US 2"
-  subnet_name         = "sql-mi-subnet"
-  subnet_cidr         = "10.0.0.0/28"
-  nsg_name            = "sql-mi-nsg"
+  location    = "West US 2"
+  subnet_name = "sql-mi-subnet"
+  subnet_cidr = "10.0.0.0/28"
+  nsg_name    = "sql-mi-nsg"
 }
 
 module "sql_mi" {
-  source              = "./Modules/cli-sql-mi"
+  source = "./Modules/cli-sql-mi"
   #admin_password      = "StrongPassword!@"
   #admin_user          = "adminuser"
   name                = "samplesqlmi"
